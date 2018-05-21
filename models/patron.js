a46d4fedd1dc711481c1b7e3c42b71989e58cc54
David Mendiola <jdmmendiola@gmail.com>
2018-05-21 01:32:42 -0400

    patron model

---
+++ models/patron.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Patron = sequelize.define('Patron', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    email: DataTypes.STRING,
+    library_id: DataTypes.STRING,
+    zip_code: DataTypes.INTEGER
+  }, {
+      timestamps: false
+  });
+  Patron.associate = function(models) {
+    // associations can be defined here
+  };
+  return Patron;
+};
