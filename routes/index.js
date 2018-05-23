36442a3ddc62fea0a931356bb18ac899dc27bcab
= <jdmmendiola@gmail.com>
2018-05-22 20:40:32 -0400

    added all route

--- routes/index.js
+++ routes/index.js
@@ -7,20 +7,34 @@ router.get('/create', function(req, res, next){
 	res.render('create', {content: {}, title: "Create"});
 });
 
+
+*/
+
+router.get('/', function(req, res, next){
+    res.redirect('/page/1');
+});
+
 router.post('/', function(req, res, next){
 	Book.create(req.body).then(function(book){
-		console.log(book);
-		res.redirect('/create');
+		res.redirect('/page/1');
 	})
 });
-*/
-router.get('/:page', function (req, res, next) {
+
+router.get('/page/:page', function (req, res, next) {
+
+    let pageLimit = 4;
+    let pageNumber = req.params.page;
+    let pageOffset = (pageNumber - 1) * pageLimit;
+
 	Book.findAndCountAll({
+
 		order: [ ['title'] ],
 		offset: ((req.params.page - 1) * pageLimit),
-		limit: pageLimit
+        limit: pageLimit
+        
 	}).then(function (book) {
-		let pages = Math.ceil(book.count / pageLimit);
+        
+        let pages = Math.ceil(book.count / pageLimit);
 
 		res.status(200).render('index', {
 			content: book.rows,
@@ -33,6 +47,16 @@ router.get('/:page', function (req, res, next) {
     });
 
 });
+
+router.get('/books/all', function(req, res, next){
+    Book.findAll({
+        order: ['title'],
+        limit: 100
+    }).then(function(book){
+        res.render('all', {content: book})
+    })
+});
+
 /*
 SELECT book_id, title AS "Book Title", first_name AS "Borrower Name", loaned_on AS "Borrowed on: " FROM loans, books, patrons WHERE book_id = 15 and loans.book_id = books.id AND loans.patron_id = patrons.id;
 */
