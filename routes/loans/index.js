00e2ad73698d2c0347efeb2f362740b7a8219699
David Mendiola <jdmmendiola@gmail.com>
2018-07-04 09:11:44 -0400

    Added loans overdue filter

--- routes/loans/index.js
+++ routes/loans/index.js
@@ -3,17 +3,47 @@ const router = express.Router();
 const Book = require('../../models').Book;
 const Loan = require('../../models').Loan;
 const Patron = require('../../models').Patron
+const { Op } = require('sequelize');
+const dayjs = require('dayjs');
 
 router.get('/all', function(req, res, next){
-    Loan.findAll({
-        order: ['id'],
-        include: [
-            {model: Patron},
-            {model: Book}
-        ]    
-    }).then(function(loan){
-        res.render('loans/all', {model: loan})
-    });
+    let today = dayjs().format('YYYY-MM-DD');
+    
+    if (req.query.filter){
+        switch(req.query.filter){
+            case 'overdue':
+                Loan.findAll({
+                    order: ['id'],
+                    include: [
+                        {model: Patron},
+                        {model: Book}
+                    ],
+                    where: {
+                        [Op.and]: {
+                            return_by: {
+                                [Op.lt]: today,
+                            },
+                            returned_on: {
+                                [Op.eq]: null
+                            }
+                        }
+                    }
+                }).then(function(loan){
+                    res.json(loan);
+                });
+                break;
+        }
+    } else {
+        Loan.findAll({
+            order: ['id'],
+            include: [
+                {model: Patron},
+                {model: Book}
+            ]    
+        }).then(function(loan){
+            res.render('loans/all', {model: loan})
+        });
+    }
 });
 
 module.exports = router;
