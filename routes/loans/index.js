cbc53273b2f67eead68541016c55627b7053304a
= <jdmmendiola@gmail.com>
2018-08-23 00:39:30 -0400

    Created loan model helper which returns a promise

--- routes/loans/index.js
+++ routes/loans/index.js
@@ -5,6 +5,7 @@ const Loan = require('../../models').Loan;
 const Patron = require('../../models').Patron
 const { Op } = require('sequelize');
 const dayjs = require('dayjs');
+let loanCreateModel = require('../../helpers/loanCreateModel');
 
 router.get('/all', function(req, res, next){
     let today = dayjs().format('YYYY-MM-DD');
@@ -75,38 +76,21 @@ router.post('/create', function(req, res, next){
         res.redirect('/loans/all');
     }).catch(function(error){
         if (error.name === 'SequelizeValidationError'){
-            res.render('loans/loan_create', {errors: error.errors});
+            let viewModel = loanCreateModel();
+            viewModel.then(function(viewModel){
+                viewModel.errors = error.errors;
+                res.render('loans/loan_create', {model: viewModel});
+            });
+            
         }
     });
 });
 
 router.get('/create', function(req, res, next){
-    
-    let viewModel = {};
-    let presentLoanDate = dayjs().format('YYYY-MM-DD');
-    let returnLoanDate = dayjs(presentLoanDate).add(7, 'day').format('YYYY-MM-DD');
-    
-    viewModel.presentLoanDate = presentLoanDate;
-    viewModel.returnLoanDate = returnLoanDate;
-
-    Book.findAll({
-        order: ['id'],
-        attributes: ['id','title']
-    }).then(function(book){
-        viewModel.bookList = book;
-        return viewModel
-    }).then(function(viewModel){
-        Patron.findAll({
-            order: ['id'],
-            attributes: ['id','first_name','last_name']
-        }).then(function(patron){
-            viewModel.patronList = patron;
-            return viewModel  
-        }).then(function(viewModel){
-            res.render('loans/loan_create', {model: viewModel})
-        });
+    let viewModel = loanCreateModel();
+    viewModel.then(function(viewModel){
+        res.render('loans/loan_create', {model: viewModel})
     });
-    
 });
 
 module.exports = router;
