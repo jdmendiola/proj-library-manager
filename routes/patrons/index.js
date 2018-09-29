7e7edd403f1e9eb1f658707f5b9561c7f7cbe1d7
David Mendiola <jdmmendiola@gmail.com>
2018-09-29 01:18:00 -0400

    File cleanup and patron pagination

--- routes/patrons/index.js
+++ routes/patrons/index.js
@@ -7,11 +7,41 @@ const { Op } = require('sequelize');
 const dayjs = require('dayjs');
 
 router.get('/', function (req, res, next) {
-    res.redirect('/books/all/1');
+    res.redirect('/patrons/all/1');
 });
 
 router.get('/all', function(req, res, next){
     res.status(200).render('patrons/all');
+});
+
+router.get('/all/:page', function (req, res, next) {
+
+    let pageLimit = 1;
+    let pageNumber = req.params.page;
+    let pageOffset = (pageNumber - 1) * pageLimit;
+
+	Patron.findAndCountAll({
+		order: [ ['id'] ],
+		offset: pageOffset,
+        limit: pageLimit
+	}).then(function (patron) {
+        let pages = Math.ceil(patron.count / pageLimit);
+
+        if (pageNumber <= pages){
+            res.status(200).render('patrons/all', {
+                model: patron.rows,
+                pagination: pages,
+                current: pageNumber,
+                filter: false
+            });
+        } else {
+            res.send('That page number does not exist.');
+        }
+		
+	}).catch(function(err){
+        res.status(500).send('Critical error');
+    });
+
 });
 
 module.exports = router;
